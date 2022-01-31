0bb68b2f34d3c34a2bfa32d3e4bda784193db329
Neil Janwani <72714187+njanwani@users.noreply.github.com>
2022-01-31 06:11:01 -0800

    Update motion_LED_Bar.ino

--- Arduino Sketches/motion_LED_Bar/motion_LED_Bar.ino
+++ Arduino Sketches/motion_LED_Bar/motion_LED_Bar.ino
@@ -6,37 +6,45 @@
 
 #include <Adafruit_NeoPixel.h>
 
-const int LED_PIN =         2;
-const int INTERRUPT_PIN =    7;
+const int LED_PIN =         2;      // Arduino digital pin for LED
+const int INTERRUPT_PIN =   7;      // Arduino motion detector interrupt pin
 
-const int LED_COUNT =       91;
-const int BRIGHTNESS =      150;
-const int ON_TIME =         15000;
+const int LED_COUNT =       91;     // Number of LEDS
+const int BRIGHTNESS =      150;    // LED brightness
+const int ON_TIME =         15000;  // How long LEDs should stay on if no motion is detected
 
-unsigned long timer =       0;
-double color =              0;
-boolean bar_on =            false;
-volatile boolean state =    false;
-volatile boolean reset =    false;
+unsigned long timer =       0;      // just counts milliseconds
+double color =              0;      // color incrementor, see color()
+boolean bar_on =            false;  // true if light's on, false otherwise
+volatile boolean state =    false;  // used to start-up LED
+volatile boolean reset =    false;  // used to reset timer
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);   // instantiate LED object
 
+// Interrupt method, runs when motions is detectted
 void motion() {
   state = true;
   reset = true;
 }
 
+// First method that is run
 void setup() {
+  // Setup motion detection
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), motion, RISING);
+   
+  // Setup strip
   strip.begin();
   strip.show();
   strip.setBrightness(BRIGHTNESS);
+   
+  // Initialize timer
   timer = millis();
 
 }
 
 void loop() {
+  // Resetting timer...
   if (reset) {
     timer = millis(); // everytime motion is detected, reset the timer
     reset = false;
@@ -44,6 +52,8 @@ void loop() {
     state = false;  // to avoid race conditions by putting this later in the code,
                     // we only want to count when we KNOW no motion has been detected
   }
+   
+  // Decides when to turn on LED
   if (state) {
     animate_on();
     for (int i = 0; i < LED_COUNT; i++) {
@@ -56,6 +66,7 @@ void loop() {
   }
 }
 
+// Animates turning on the LED strip
 void animate_on() {
   for (int i = 0; i < LED_COUNT / 2 && !bar_on; i++) {
     strip.setPixelColor(i + LED_COUNT / 2, trig(color), trig(color + 2 * PI / 3), trig(color + 4 * PI / 3));
@@ -66,6 +77,7 @@ void animate_on() {
   bar_on = true;
 }
 
+// Animates turning off the LED strip
 void animate_off() {
   bar_on = false;
   for (int i = 0; i <= LED_COUNT / 2 + 1; i++) {
@@ -76,10 +88,12 @@ void animate_off() {
   }
 }
 
+// Handles color modulation
 int trig(double color) {
   return 127 * sin(color) + 127;
 }
 
+// Simply increments color
 void increment_color() {
   color += 0.001;
   if (color >= 2 * PI) {
